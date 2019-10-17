8da75b95537aa7ae4242cc58ed858b91efa34ec0
zhaotq <306403031@qq.com>
2019-10-17 18:25:06 +0800

    添加随访事件接口

--- src/main/java/com/dn/wx/controller/FollowEventController.java
+++ src/main/java/com/dn/wx/controller/FollowEventController.java
@@ -1,9 +1,15 @@
 package com.dn.wx.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.dn.wx.entity.FollowEvent;
+import com.dn.wx.service.IFollowEventService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
 import com.dn.common.controller.BaseController;
 
 /**
@@ -18,4 +24,50 @@ import com.dn.common.controller.BaseController;
 @RequestMapping("/api/wx/follow-event")
 public class FollowEventController extends BaseController {
 
+    @Autowired
+    private IFollowEventService followEventService ;
+
+    /**
+     * 获取所有的随访类型
+     * @param pageSize
+     * @param current
+     * @param eventType
+     * @param eventName
+     * @return
+     */
+    @GetMapping
+    public IPage<FollowEvent> getFollowEventPages(@RequestParam(required = false,defaultValue = "20") long pageSize,
+                                                  @RequestParam(required = false,defaultValue = "1") long current,
+                                                  @RequestParam(required = false)String eventType,
+                                                  @RequestParam(required = false)String eventName){
+        QueryWrapper<FollowEvent> queryWrapper = new QueryWrapper<>();
+        if(StringUtils.isNotBlank(eventName) && StringUtils.isNotEmpty(eventName)){
+            queryWrapper.like("event_name",eventName);
+        }
+        if(StringUtils.isNotEmpty(eventType)&&StringUtils.isNotBlank(eventType)){
+            queryWrapper.eq("event_type",eventType);
+        }
+        return followEventService.page(new Page<>(current,pageSize),queryWrapper);
+    }
+
+
+    /**
+     * 增加或者修改
+     * @param followEvent
+     * @return
+     */
+    @PostMapping
+    public boolean saveOrUpdateFollowEvent(@RequestBody FollowEvent followEvent){
+        return followEventService.saveOrUpdate(followEvent);
+    }
+
+    /**
+     * 删除
+     * @param id
+     * @return
+     */
+    @DeleteMapping
+    public boolean deleteFollowEvent(String id){
+        return followEventService.removeById(id) ;
+    }
 }
--- src/main/java/com/dn/wx/entity/FollowEvent.java
+++ src/main/java/com/dn/wx/entity/FollowEvent.java
@@ -1,46 +1,46 @@
 package com.dn.wx.entity;
 
-    import com.dn.common.domain.BaseDomain;
-    import com.baomidou.mybatisplus.annotation.TableName;
-    import io.swagger.annotations.ApiModel;
-    import io.swagger.annotations.ApiModelProperty;
-    import lombok.Data;
-    import lombok.EqualsAndHashCode;
-    import lombok.experimental.Accessors;
+import com.dn.common.domain.BaseDomain;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
 
 /**
-* <p>
-    * 随访事件
-    * </p>
-*
-* @author dn
-* @since 2019-09-23
-*/
-    @Data
-        @EqualsAndHashCode(callSuper = true)
-    @Accessors(chain = true)
-    @TableName("wx_follow_event")
-    @ApiModel(value="FollowEvent对象", description="随访事件")
-    public class FollowEvent extends BaseDomain {
+ * <p>
+ * 随访事件
+ * </p>
+ *
+ * @author dn
+ * @since 2019-09-23
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Accessors(chain = true)
+@TableName("wx_follow_event")
+@ApiModel(value = "FollowEvent对象", description = "随访事件")
+public class FollowEvent extends BaseDomain {
 
     private static final long serialVersionUID = 1L;
 
-            @ApiModelProperty(value = "事件类型 发送文本、填写表单")
-    private String enventType;
+    @ApiModelProperty(value = "事件类型 发送文本、填写表单")
+    private String eventType;
 
-            @ApiModelProperty(value = "表单链接 填写表单类型的地址")
+    @ApiModelProperty(value = "表单链接 填写表单类型的地址")
     private String eventUrl;
 
-            @ApiModelProperty(value = "事件名称")
+    @ApiModelProperty(value = "事件名称")
     private String eventName;
 
-            @ApiModelProperty(value = "事件DOM")
+    @ApiModelProperty(value = "事件DOM")
     private String eventDom;
 
-            @ApiModelProperty(value = "事件内容 事件类型为文本内容")
+    @ApiModelProperty(value = "事件内容 事件类型为文本内容")
     private String eventContent;
 
-            @ApiModelProperty(value = "获取事件API接口")
+    @ApiModelProperty(value = "获取事件API接口")
     private String dataApi;
 
 
